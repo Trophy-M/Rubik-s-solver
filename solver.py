569357e8c6af47e8e240a330072e54dbae78931d
Trophy-M <115034674+Trophy-M@users.noreply.github.com>
2022-10-05 12:06:08 +0100

    Update solver.py

---
+++ solver.py
@@ -0,0 +1,11 @@
+from cube import *
+
+def getcolorpos(Rubikcube, color, solved):
+
+def whitecross(Rubikcube):
+    isWhitecross = False
+    while isWhitecross == False:
+        if [Rubikcube.cu[0][1],Rubikcube.cu[0][3],Rubikcube.cu[0][4],Rubikcube.cu[0][5],Rubikcube.cu[0][7]] == ['u2','u4','u5','u6','u8']:
+            isWhiteCross = True
+
+
