2c543867267a84bb23287110f14245b5d486dc40
AsawinM <asawinwisittrophy@gmail.com>
2022-11-14 23:09:46 +0000

    k

--- movelibrary.py
+++ movelibrary.py
@@ -1,4 +1,5 @@
-standardmoves = ['u','l','f','r','b','d','up','lp','fp','rp','bp','dp','u2','l2','f2','r2','b2','d2']
+#standardmoves = ['u','l','f','r','b','d','up','lp','fp','rp','bp','dp','u2','l2','f2','r2','b2','d2']
+standardmoves = ['u','l','f','r','b','d','up','lp','fp','rp','bp','dp']
 whitecrossmoves = [['RP','DP','R'],['F','D','FP'],['F','L','D2','LP','FP']]
 
 #cross 1 - maketurns(Rubikcube, ['U','F','BP','L2','U2','L2','FP','B','U2','L2','U'])
